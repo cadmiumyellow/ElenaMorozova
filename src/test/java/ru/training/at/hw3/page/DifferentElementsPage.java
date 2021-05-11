f70cf7c6fd4e9648037048de4e031cac03e270e3
Elena Morozova <helen-jan@mail.ru>
2021-05-11 17:26:53 +0300

    hw3

---
+++ src/test/java/ru/training/at/hw3/page/DifferentElementsPage.java
@@ -0,0 +1,69 @@
+package ru.training.at.hw3.page;
+
+import ru.training.at.hw3.utils.WaitActions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class DifferentElementsPage {
+
+    private WaitActions waitActions;
+    private WebDriver webDriver;
+    private WebElement webElement;
+
+
+    @FindBy(css = ".label-checkbox:first-child > input")
+    private WebElement waterCheckbox;
+
+    @FindBy(css = ".label-checkbox:nth-last-child(2) > input")
+    private WebElement windCheckbox;
+
+    @FindBy(css = ".label-radio:last-child > input")
+    private WebElement selenRadio;
+
+    @FindBy(css = ".colors")
+    private WebElement colorsDropDown;
+
+    @FindBy(xpath = "//*[text()='Yellow']")
+    private WebElement yellow;
+
+    @FindBy(css = "ul.panel-body-list > li")
+    private List<WebElement> logs;
+
+
+    @Inject
+    public DifferentElementsPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
+        waitActions = new WaitActions(webDriver);
+    }
+
+    public String getCurrentUrl() {
+        return webDriver.getCurrentUrl();
+    }
+
+    public Boolean areCheckboxesSelected() {
+        waterCheckbox.click();
+        windCheckbox.click();
+        return ((waterCheckbox.isSelected()) && (windCheckbox.isSelected()));
+    }
+
+    public Boolean isSelenRadioSelected() {
+        selenRadio.click();
+        return selenRadio.isSelected();
+    }
+
+    public Boolean isYellowSelected() {
+        colorsDropDown.click();
+        yellow.click();
+        return yellow.isSelected();
+    }
+
+    public List<WebElement> getLogs() {
+        return logs;
+    }
+}
